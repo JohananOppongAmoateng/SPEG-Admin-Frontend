e91fe40d5e2a699f44fecb03087a6372cfe987de
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2025-07-21 13:43:36 +0000

    Update UserDetailsModal.tsx

--- src/app/dashboard/users/UserDetailsModal.tsx
+++ src/app/dashboard/users/UserDetailsModal.tsx
@@ -38,7 +38,7 @@ const UserDetailsModal = ({ user, onClose, markVerified}: any) => {
 
         setIsDeleting(true);
         try {
-      const result = await deleteUser(userId);
+      const result = await deleteUser(user.id);
       if (result.success) {
         toast.success("User deleted successfully");
         onClose();
